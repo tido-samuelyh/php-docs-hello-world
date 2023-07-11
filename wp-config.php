d445118738e3a2e1bf80245598d384226e06f457
samuelyh <samuelyh@hku.hk>
2023-07-11 17:04:14 +0800

    out

--- wp-config.php
+++ wp-config.php
@@ -94,3 +94,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
+define ('WP_CONTENT_URL', 'http://wpsubdir.dev' . '/wp-content' );
